3fe966f86e7f121c3a58864bfade9c1997d6f6ec
Abdelrhman-sherif <abdelrhman.sherif3011@gmail.com>
2025-09-05 14:55:12 +0300

    Update serializers.py

--- api_order/serializers.py
+++ api_order/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Product
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    subtotal = serializers.SerializerMethodField()
+
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        fields = ['product', 'product_name', 'quantity', 'price', 'subtotal']
+
+    def get_subtotal(self, obj):
+        return obj.quantity * obj.price
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+
     class Meta:
         model = Order
-        fields = ['id', 'user', 'created_at', 'status', 'items']
+        fields = ['id', 'user', 'full_name', 'email', 'address', 'city', 'phone', 'status', 'created_at', 'total', 'items']
