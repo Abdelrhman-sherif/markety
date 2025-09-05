eb23371adf79e3d8b24cb34fbb3cb1f68cf4e6c9
Abdelrhman-sherif <abdelrhman.sherif3011@gmail.com>
2025-09-05 15:06:51 +0300

    Update views.py

--- api_order/views.py
+++ api_order/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Order
 from .serializers import OrderSerializer
 
 class OrderDetailView(generics.RetrieveAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
